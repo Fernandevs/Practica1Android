1c616168f2a18b3674479f4af8e45aa87c3c82ff
fernandevs <ortizfernando05@gmail.com>
2023-02-27 10:53:46 -0600

    Implementación de Toast en las imágenes del FirstFragment.kt

--- app/src/main/java/com/example/practica1android/FirstFragment.kt
+++ app/src/main/java/com/example/practica1android/FirstFragment.kt
@@ -1,19 +1,20 @@
 package com.example.practica1android
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+
 import com.example.practica1android.databinding.FragmentFirstBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
-
     private var _binding: FragmentFirstBinding? = null
 
     // This property is only valid between onCreateView and
